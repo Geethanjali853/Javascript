105514c737d772df6eb2723423b1f3cd7af52127
Geethanjali853 <anjaligeetha760@gmail.com>
2024-04-14 20:17:06 +0530

    Task

--- Assignment/Conditional Statement/Descending.js
+++ Assignment/Conditional Statement/Descending.js
@@ -1,3 +1,9 @@
 var  a=[10,20,30,40]
 a.reverse()
 console.log(a)
+
+
+var arr = [45, 89, 78, 23, -12, 10, 32, 6];
+console.log("Unsorted Array:", arr);
+arr.sort((a, b) => b - a);
+console.log("Sorted Array:", arr);
